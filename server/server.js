e7b27fc61e035b1c6de653f91f89fb7331c5bf60
Orion Palmer <opalmer1989@gmail.com>
2022-10-02 17:00:53 -0400

    initial commit

---
+++ server/server.js
@@ -0,0 +1,70 @@
+require("dotenv").config();
+const express  = require("express");
+const mongoose = require("mongoose");
+// const cors     = require("cors");
+const path     = require("path");
+const app      = express();
+ 
+const PORT     = process.env.PORT || 4747;
+const DB_URI   = "mongodb://localhost:27017/"
+const DB       = "reactDB";
+ 
+// Middleware
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+// app.use(cors());
+ 
+// Establish DB connection
+mongoose.connect(DB_URI + DB, {
+   useUnifiedTopology: true,
+   useNewUrlParser: true,
+//    useCreateIndex: true,
+//    useFindAndModify: false,
+   connectTimeoutMS: 10000
+});
+ 
+const db = mongoose.connection;
+ 
+// Event listeners
+db.once('open', () => console.log(`Connected to ${DB} database`));
+ 
+// Create Schema
+let PersonSchema = new mongoose.Schema(
+   {
+      forename: String,
+      age: Number
+   },
+   { collection: "people" }
+);
+ 
+// Create Model
+let PersonModel = db.model("PersonModel", PersonSchema);
+ 
+// Route to Get all People
+app.get("/api/people", (req, res) => {
+   PersonModel.find({}, {__v: 0}, (err, docs) => {
+      if (!err) {
+         res.json(docs);
+      } else {
+         res.status(400).json({"error": err});
+      }
+   });
+})
+ 
+// Route to Add a Person
+app.post("/api/person/add", (req, res) => {
+   let person = new PersonModel(req.body);
+   
+   person.save((err, result) => {
+      if (!err) {
+         delete result._doc.__v;
+         res.json(result._doc);
+      } else {
+         res.status(400).json({"error": err});
+      }
+   });
+})
+ 
+app.listen(PORT, () => {
+   console.log(app.get("env").toUpperCase() + " Server started on port " + (PORT));
+});
